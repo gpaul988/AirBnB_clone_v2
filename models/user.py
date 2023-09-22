cd3babe193e9e9134eaf51ce4ee68fb55195d1d8
gpaul988 <gpaul988@gmail.com>
2023-09-22 13:16:50 -0700

    all done

--- models/user.py
+++ models/user.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 # Graham S. Paul (user.py)
 """This module defines a class User"""
+import hashlib
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 from models.stringtemplates import HBNB_TYPE_STORAGE, DB
 from sqlalchemy import Column, String
@@ -21,7 +27,47 @@ class User(BaseModel, Base):
         reviews = relationship('Review', backref='user',
                                cascade='all, delete, delete-orphan')
     else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = '' 
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+     def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, myemail@gmail.com):
+        """email values"""
+        self._email = myemail@gmail.com
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = pwd
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, John):
+        """first_name values"""
+        self._first_name = John
+
+     @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, Doe)
+        """last_name values"""
+        self._last_name = Doe
