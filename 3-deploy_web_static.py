6f2740b3630f0a4e1a4a055da6d038831130e163
gpaul988 <gpaul988@gmail.com>
2023-10-04 14:56:49 -0700

    completed

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
 """ Graham S. Paul (3-deploy_web_static.py) - Share archive to web servers, using function do_deploy """
-from fabric.contrib import files
 from fabric.api import env, put, run, local
-import time
-import os
-
+import os.path
+from time import strftime
 env.hosts = ['54.82.216.202', '18.234.106.116']
 
 
 def do_pack():
-    """Gerenate tgz."""
-    timestamp = time.strftime("%Y%m%d%H%M%S")
+    '''Generate required files'''
+    timenow = strftime('%Y%M%d%H%M%S')
     try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
-              format(timestamp))
-        return ("versions/web_static_{:s}.tgz".format(timestamp))
-    except:
+        local('mkdir -p versions')
+        filename = 'versions/web_static_{}.tgz'.format(timenow)
+        local('tar -czvf {} web_static/'.format(filename))
+        return filename
+    except Exception:
         return None
 
 
 def do_deploy(archive_path):
-    """Function for deploy."""
-    if not os.path.exists(archive_path):
+    '''Upload achive to web servers'''
+    if not os.path.isfile(archive_path):
         return False
-
-    data_path = '/data/web_static/releases/'
-    tmp = archive_path.split('.')[0]
-    name = tmp.split('/')[1]
-    dest = data_path + name
+    try:
+        filename = archive_path.split('/')[-1]
+        no_ext = filename.split('.')[0]
