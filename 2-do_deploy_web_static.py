722f5ef10b261ef241d483f50b99e9ac21d4fb7a
gpaul988 <gpaul988@gmail.com>
2023-10-04 14:39:38 -0700

    completed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 # Graham S. Paul (2-do_deploy_web_static.py)
-'''
-Sends files to remote server using Fabric
-'''
+from fabric.contrib import files
 from fabric.api import env, put, run
-import os.path
-env.hosts = ['web1.gpaul.info', 'web2.gpaul.info']
+import os
+
+env.hosts = ['54.82.216.202', '18.234.106.116']
 
 
 def do_deploy(archive_path):
-    '''Push achive to web servers'''
-    if not os.path.isfile(archive_path):
+    """Function for deploy"""
+    if not os.path.exists(archive_path):
         return False
+
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
+
     try:
-        filename = archive_path.split('/')[-1]
-        no_ext = filename.split('.')[0]
-        path_no_ext = '/data/web_static/releases/{}/'.format(no_ext)
-        symlink = '/data/web_static/current'
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(path_no_ext))
-        run('tar -xzf /tmp/{} -C {}'.format(filename, path_no_ext))
-        run('rm /tmp/{}'.format(filename))
-        run('mv {}web_static/* {}'.format(path_no_ext, path_no_ext))
-        run('rm -rf {}web_static'.format(path_no_ext))
-        run('rm -rf {}'.format(symlink))
-        run('ln -s {} {}'.format(path_no_ext, symlink))
+        put(archive_path, '/tmp')
+        run('sudo mkdir -p {}'.format(dest))
+        run('sudo tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('sudo rm -f /tmp/{}.tgz'.format(name))
+        run('sudo mv {}/web_static/* {}/'.format(dest, dest))
+        run('sudo rm -rf {}/web_static'.format(dest))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(dest))
         return True
-    except Exception:
+    except:
         return False
